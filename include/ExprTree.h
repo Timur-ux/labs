c67f728f0f0578971af903c7988f269b92852a7d
Tias <timur.buchkin@mail.ru>
2026-04-06 11:12:13 +0300

    add expr tree creation and printing

--- include/ExprTree.h
+++ include/ExprTree.h
@@ -6,4 +6,12 @@
 
 STACK(Tokens, TokenVoid);
 Tokens DijkstraToPrefix(CharStream *stream);
+
+typedef struct ExprTree ExprTree;
+
+ExprTree *ExprTree_Create(Tokens *tokens);
+void ExprTree_Destroy(ExprTree * tree);
+
+void ExprTree_Print(ExprTree* tree, FILE *file);
+void ExprTree_Differentiate(ExprTree *tree, char var);
 #endif // !EXPR_TREE_H_
--- src/ExprTree.c
+++ src/ExprTree.c
@@ -1,6 +1,7 @@
 #include "ExprTree.h"
 #include "Token.h"
 #include "Utils.h"
+#include <stdlib.h>
 
 void DijkstraToPrefix_impl(CharStream *stream, Tokens *result, Tokens *temp);
 Tokens DijkstraToPrefix(CharStream *stream) {
@@ -56,7 +57,7 @@ void DijkstraToPrefix_impl(CharStream *stream, Tokens *result, Tokens *temp) {
     case TOKEN_NUMBER:
     case TOKEN_VAR:
       Tokens_Push(result, token);
-			break;
+      break;
     case TOKEN_OP:
       op = Token_GetOp(token);
       if (op == OP_BRACKET_OPEN) {
@@ -78,9 +79,90 @@ void DijkstraToPrefix_impl(CharStream *stream, Tokens *result, Tokens *temp) {
         Tokens_Push(result, *Tokens_Top(temp)), Tokens_Pop(temp);
 
       Tokens_Push(temp, token);
-			break;
+      break;
     }
   }
-	while (!Tokens_Empty(temp))
-		Tokens_Push(result, *Tokens_Top(temp)), Tokens_Pop(temp);
+  while (!Tokens_Empty(temp))
+    Tokens_Push(result, *Tokens_Top(temp)), Tokens_Pop(temp);
 }
+
+typedef struct Node {
+  TokenVoid token;
+  struct Node *left, *right;
+} Node;
+
+struct ExprTree {
+  Node *root;
+};
+
+Node *Node_Create(Tokens *tokens) {
+  if (Tokens_Empty(tokens))
+    FatalError("Expected non empty stack");
+
+  Node *node = (Node *)malloc(sizeof(Node));
+  node->left = node->right = NULL;
+  TokenVoid token = *Tokens_Top(tokens);
+  Tokens_Pop(tokens);
+  node->token = token;
+  if (token.type == TOKEN_NUMBER || token.type == TOKEN_VAR)
+    return node;
+
+  node->right = Node_Create(tokens);
+  if (Token_GetOp(token) != OP_MINUS_UNARY)
+    node->left = Node_Create(tokens);
+
+  return node;
+}
+
+ExprTree *ExprTree_Create(Tokens *tokens) {
+  ExprTree *tree = (ExprTree *)malloc(sizeof(ExprTree));
+  tree->root = Node_Create(tokens);
+  return tree;
+}
+
+void Node_Destroy(Node *node) {
+  if (!node)
+    return;
+  Node_Destroy(node->left);
+  Node_Destroy(node->right);
+
+  Token_Destroy(&node->token);
+  free(node);
+}
+void ExprTree_Destroy(ExprTree *tree) {
+  Node_Destroy(tree->root);
+  free(tree);
+}
+
+bool IsBracketsWrapNeed(TokenVoid parent, TokenVoid child) {
+  return parent.type == TOKEN_OP && child.type == TOKEN_OP &&
+         GetOpPriority(Token_GetOp(parent)) > GetOpPriority(Token_GetOp(child));
+}
+void Node_Print(Node *node, FILE *file) {
+  if (!node)
+    return;
+
+  if (node->left && IsBracketsWrapNeed(node->token, node->left->token)) {
+		fprintf(file, "(");
+		Node_Print(node->left, file);
+		fprintf(file, ")");
+	} else 
+		Node_Print(node->left, file);
+
+	fprintf(file, " ");
+	Token_Print(node->token, file);
+	fprintf(file, " ");
+
+  if (node->right && IsBracketsWrapNeed(node->token, node->right->token)) {
+		fprintf(file, "(");
+		Node_Print(node->right, file);
+		fprintf(file, ")");
+	} else 
+		Node_Print(node->right, file);
+}
+
+void ExprTree_Print(ExprTree *tree, FILE *file) {
+	Node_Print(tree->root, file);
+}
+
+void ExprTree_Differentiate(ExprTree *tree, char var);
